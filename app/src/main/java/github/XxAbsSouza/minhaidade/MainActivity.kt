ebb1dc07fdf12d569e03254b0f5eafd71360b8a2
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2024-10-28 21:48:37 -0300

    declaração declarativo

---
+++ app/src/main/java/github/XxAbsSouza/minhaidade/MainActivity.kt
@@ -0,0 +1,53 @@
+package github.XxAbsSouza.minhaidade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import github.XxAbsSouza.minhaidade.ui.theme.MinhaidadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MinhaidadeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CounterScreen(){
+    Column() {
+        Text(text = "Qual a sua idade?")
+        Text(text = "Aperte os botões para informar a sua idade.")
+        Text(text = "21")
+
+        Row() {
+            Button(onClick = {}) {
+                Text(text = "-")
+            }
+            Button(onClick = {}) {
+                Text(text = "+")
+            }
+        }
+
+    }
+}
